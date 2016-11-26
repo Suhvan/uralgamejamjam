bbc5dd441e419867799c3823a234439e499c2e7e
Suhvan <soohvan@gmail.com>
2016-11-26 20:31:15 +0500

    fixed light overlap

--- ugj2/Assets/Src/Maze/MazeCellEdge .cs
+++ ugj2/Assets/Src/Maze/MazeCellEdge .cs
@@ -16,7 +16,7 @@ public abstract class MazeCellEdge : MonoBehaviour
 		transform.parent = cell.transform ;
 		transform.localPosition = Vector3.zero;
 		transform.localRotation = direction.ToRotation();
-		transform.position = new Vector3(transform.position.x, transform.position.y, 0.01f * (1 + (int)direction));
+		transform.position = new Vector3(transform.position.x, transform.position.y, 0.01f * (1 + (int)direction) + (float)(cell.coordinates.x + cell.coordinates.y) * 0.00001f);
 	}
 
 	public abstract void EnableRoomSettings();
